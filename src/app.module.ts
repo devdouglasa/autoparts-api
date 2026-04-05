b416dd2bf4a9e44c39b74dde7e2fca7a4af079dd
devdouglasa <douglasalisson27@gmail.com>
2026-04-05 11:15:13 -0300

    feat: added SuppliersService dependency injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { SuppliersService } from './suppliers/suppliers.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, SuppliersService],
 })
 export class AppModule {}
